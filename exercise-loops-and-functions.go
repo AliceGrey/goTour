7684bbd6baaba28a66d9aa0c6c5ed873c38c3d00
AliceGrey <almw@protonmail.com>
2019-09-28 12:38:39 -0700

    Second iteration of Loops and Functions
    Next, change the loop condition to stop once the value has stopped changing (or only changes by a very small amount). See if that's more or fewer than 10 iterations.

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -5,14 +5,14 @@ import (
 )
 
 func Sqrt(x float64) float64 {
-	
+
 	z := 1.0
-	
+
 	for i := 0; i < 10; i++ {
 		z -=  (z*z - x) / (2*z)
 		fmt.Println(z*z)
+		if z*z - x <= 0.001 {break}
 	}
-	
 	return z
 }
 
