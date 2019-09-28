a643f3fc148958777ff3594c992d747e45676616
AliceGrey <almw@protonmail.com>
2019-09-28 12:25:15 -0700

    First iteration of Loops and Functions
    "Implement this in the func Sqrt provided. A decent starting guess for z is 1, no matter what the input. To begin with, repeat the calculation 10 times and print each z along the way. "

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -5,10 +5,18 @@ import (
 )
 
 func Sqrt(x float64) float64 {
+	
+	z := 1.0
+	
+	for i := 0; i < 10; i++ {
+		z -=  (z*z - x) / (2*z)
+		fmt.Println(z*z)
+	}
+	
+	return z
 }
 
 
 func main() {
 	fmt.Println(Sqrt(2))
 }
-
